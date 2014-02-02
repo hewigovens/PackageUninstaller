63ae859814787a85675b1ba106e041a221c7cdd3
hewigovens <hewigovens@gmail.com>
2014-02-02 19:24:41 +0800

    add sparkle

--- PackageUninstallerApp/AppDelegate.m
+++ PackageUninstallerApp/AppDelegate.m
@@ -11,25 +11,32 @@
 #import <SecurityInterface/SFAuthorizationView.h>
 
 #import <Crashlytics/Crashlytics.h>
+#import <Sparkle/Sparkle.h>
 
 @implementation AppDelegate
 
-- (void)awakeFromNib{
+- (void)awakeFromNib
+{
     packageListController = [[PackagesListViewController alloc] initWithNibName:@"PackagesListViewController" bundle:[NSBundle mainBundle]];
     self.window.contentView = [packageListController view];
     self.window.delegate = self;
     self.window.title = NSLocalizedStringFromTable(@"CFBundleName", @"InfoPlist", "General Package Uninstaller");
 }
 
-- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag{
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    [Crashlytics startWithAPIKey:@"00294b074c27a6569db329a72df442fbff108a8c"];
+}
+
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag
+{
     [self.window makeKeyAndOrderFront:self];
     return YES;
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
-    [Crashlytics startWithAPIKey:@"00294b074c27a6569db329a72df442fbff108a8c"];
+-(IBAction)checkUpdates:(id)sender
+{
+    [[SUUpdater sharedUpdater] checkForUpdates:sender];
 }
-
-
 
 @end
